53b30b6346d8421249d209f2cbdaf26e3c999ea7
bhavesh <bhavesh.badjatya@gmail.com>
2017-12-01 17:33:45 +0530

    dummy scipts file

---
+++ python_scripts/LR_debug3.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import h5py
+import scipy
+from PIL import Image
+from scipy import ndimage
+import re
+import sys
+import time
+def load_dataset():
+    train_dataset = h5py.File('/apps/AI_Tools/current/datasets/train_catvnoncat.h5', "r")
+    train_set_x_orig = np.array(train_dataset["train_set_x"][:]) # your train set features
+    train_set_y_orig = np.array(train_dataset["train_set_y"][:]) # your train set labels
+    test_dataset = h5py.File('/apps/AI_Tools/current/datasets/test_catvnoncat.h5', "r")
+    test_set_x_orig = np.array(test_dataset["test_set_x"][:]) # your test set features
+    test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # your test set labels
+    classes = np.array(test_dataset["list_classes"][:]) # the list of classes
+    train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
+    test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
+    return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
+train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
---
+++ python_scripts/LR_debug4.py
@@ -0,0 +1,4 @@
+import numpy as np
+f = open("dummy_file4.txt","w+")
+f.write("dummy4")
+f.close()
